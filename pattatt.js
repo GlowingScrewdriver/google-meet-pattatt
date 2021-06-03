98da0d67b5f0dbeb6e2649046706fba2cb2183ad
Vedanth Padmaraman <vedanthpadmaraman0566@gmail.com>
2021-06-03 13:41:05 +0530

    Update pattatt.js

--- pattatt.js
+++ pattatt.js
@@ -1,7 +1,7 @@
 document.onkeydown = function um (k) {
 	if (k.key === ' ') {
 		if (document.activeElement === document.body) {
-			document.querySelectorAll("[data-tooltip=\"Turn on microphone (CTRL + D)\"], [data-tooltip=\"Turn on microphone (ctrl + d)\"]")[0].click();
+			document.querySelector("[data-tooltip=\"Turn on microphone (CTRL + D)\"], [data-tooltip=\"Turn on microphone (ctrl + d)\"], [aria-label=\"Turn on microphone (CTRL + D)\"]").click();
 			document.activeElement.blur();
 		}
 	}
@@ -10,14 +10,8 @@ document.onkeydown = function um (k) {
 document.onkeyup = function m (l) {
 	if (l.key === ' ') {
 		if (document.activeElement === document.body) {
-			document.querySelectorAll("[data-tooltip=\"Turn off microphone (CTRL + D)\"], [data-tooltip=\"Turn off microphone (ctrl + d)\"]")[0].click();
+			document.querySelector("[data-tooltip=\"Turn off microphone (CTRL + D)\"], [data-tooltip=\"Turn off microphone (ctrl + d)\"], [aria-label=\"Turn off microphone (CTRL + D)\"]").click();
 			document.activeElement.blur();
 		}
 	}
 }
-
-//		<div class="MvD9Jd ncMv2e" style="margin: auto; top: -88px;">
-//			<div class="dj3AKc RYvOkb">
-//				<p class="McXdrf">Click an empty part of the bottom bar before using the spacebar.</p>
-//			</div>
-//		</div>
